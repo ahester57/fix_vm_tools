00cef2724df1e3f58e7d356d5d3b4caff2be7b67
Austin Hester <ahester57@gmail.com>
2019-04-15 10:52:46 -0400

    update script to fix most other problems
    Signed-off-by: Austin Hester <ahester57@gmail.com>

--- fix_vm_tools.sh
+++ fix_vm_tools.sh
@@ -1,28 +1,60 @@
 #!/bin/bash
 
+reinstall () {
+    while true; do
+        read -p "Reinstall VM tools now? " answer
+        case $answer in
+            y|Y|yes|YES)
+                echo "Reinstalling..." &&
+                sudo apt update &&
+                sudo apt install --reinstall open-vm-tools open-vm-tools-desktop
+                break;;
+            n|N|no|NO)
+                break;;
+            *)
+                echo "(y/n) ";;
+        esac
+
+    done
+}
+
+reboot () {
+    while true; do
+        read -p "Reboot Now? " answer
+        case $answer in
+            y|Y|yes|YES)
+                echo "Rebooting..." &&
+                sudo reboot -i NOW
+                break;;
+            n|N|no|NO)
+                break;;
+            *)
+                echo "(y/n) ";;
+        esac
+
+    done
+}
+
+
 while true; do
-    read -p "Reinstall VM tools now? " answer
+    read -p "Is VM tools broken? " answer
     case $answer in
         y|Y|yes|YES)
-            echo "Reinstalling..." &&
-            sudo apt update &&
-            sudo apt install --reinstall open-vm-tools open-vm-tools-desktop
-            break;;
+            reinstall
+            reboot
+            exit;;
         n|N|no|NO)
             break;;
         *)
             echo "(y/n) ";;
     esac
-
 done
 
-
 while true; do
-    read -p "Reboot Now? " answer
+    read -p "Is something else broken? " answer
     case $answer in
         y|Y|yes|YES)
-            echo "Rebooting..." &&
-            sudo reboot -i NOW
+            reboot
             break;;
         n|N|no|NO)
             break;;
